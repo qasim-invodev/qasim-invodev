dd5cf64f40f0ab97d63d846483582580de7bc937
Qasim Mehmood <qasimmehmood@MP17-QasimM-SE-418.local>
2022-06-22 12:29:18 +0500

    Functions Done

--- elix_funcs.exs
+++ elix_funcs.exs
@@ -1,6 +1,17 @@
+#Practice of Functions and Capture Operators in Elixix
 
 
-f_to_c = fn(f)-> (f - 32)/1.8 end
+#Function Definations
+f_to_c = fn(f)-> IO.puts((f - 32)/1.8) end
 
-seconds = fn(day)-> day*24*60*60 end
+seconds = fn(day)-> IO.puts(day*24*60*60) end
 
+
+#Function Calls
+seconds.(4)
+f_to_c.(103)
+
+#A new function that takes 2 argument maps and returns average age using Capture Operator
+cal_avg = &(IO.puts((&1.age + &2.age)/2))
+
+cal_avg.(%{age: 20},%{age: 30})
